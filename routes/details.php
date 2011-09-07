fa2951cde7b0ca8b42afb5853b9bf7cc399b583f
Adam Fairholm <adam.fairholm@gmail.com>
2011-09-06 22:52:23 -0400

    Details file.

---
+++ routes/details.php
@@ -0,0 +1,86 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * PyroRoutes Details File
+ *
+ * @package  	PyroCMS
+ * @subpackage  PyroChunks
+ * @category  	Details
+ * @author  	Parse19 (Adam Fairholm)
+ */ 
+class Module_Routes extends Module {
+
+	public $version = '0.9';
+	
+	public $db_pre;
+
+ 	// --------------------------------------------------------------------------
+
+	public function __construct()
+	{	
+		if(CMS_VERSION >= 1.3)
+			$this->db_pre = SITE_REF.'_';
+	}
+
+	// --------------------------------------------------------------------------
+	
+ 	public function info()
+	{
+		return array(
+		    'name' => array(
+		        'en' => 'Routes'
+		    ),
+		    'description' => array(
+		        'en' => 'Manage custom routes.'
+		    ),
+		    'frontend' => false,
+			'backend' => true,
+			'menu' => 'utilities',
+			'author' => 'Parse19'
+		);
+	}
+
+	// --------------------------------------------------------------------------
+
+	public function install()
+	{
+		$sql = "
+            CREATE TABLE IF NOT EXISTS `{$this->db_pre}routes` (
+                `id` int(11) NOT NULL AUTO_INCREMENT,
+                `route_key` varchar(200) NOT NULL,
+                `route_value` varchar(200) NOT NULL,
+                `notes` text,
+                `when_added` datetime DEFAULT NULL,
+                `last_updated` datetime DEFAULT NULL,
+                `added_by` int(11) DEFAULT NULL,
+                PRIMARY KEY (`id`)
+              ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+              
+		return $this->db->query($sql);
+	}
+
+	// --------------------------------------------------------------------------
+
+	public function uninstall()
+	{
+		$this->load->dbforge();
+		
+		return $this->dbforge->drop_table('routes');
+	}
+
+	// --------------------------------------------------------------------------
+
+	public function upgrade($old_version)
+	{
+		return true;
+	}
+
+	// --------------------------------------------------------------------------
+
+	public function help()
+	{
+		return "No documentation has been added for this module.<br/>Contact the module developer for assistance.";
+	}
+}
+
+/* End of file details.php */
