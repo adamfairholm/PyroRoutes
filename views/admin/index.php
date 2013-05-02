032d4b7421a9e80badd319b14c7a69be5bfe8d85
Antoine Benevaut <antoine.benevaut@gmail.com>
2013-05-02 17:34:59 +0200

    Adding div content.

--- views/admin/index.php
+++ views/admin/index.php
@@ -3,37 +3,37 @@
 </section>
 
 <section class="item">
+	<div class="content">
+		<?php if (!empty($routes)): ?>
 
-<?php if (!empty($routes)): ?>
+		<table class="table-list">
+			<thead>
+				<tr>
+				    <th><?php echo lang('pyroroutes.route_name');?></th>
+				    <th><?php echo lang('pyroroutes.route_key');?></th>
+				    <th><?php echo lang('pyroroutes.route_value');?></th>
+				    <th></th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php foreach ($routes as $route):?>
+				<tr>
+					<td><strong><?php echo $route->name; ?></strong></td>
+					<td><?php echo $route->route_key; ?></td>
+					<td><?php echo $route->route_value; ?></td>
+					<td class="actions"> 
+						<a href="<?php echo site_url('admin/routes/edit_route/'.$route->id);?>" class="button edit">Edit</a>
+						<a href="<?php echo site_url('admin/routes/delete_route/'.$route->id);?>" class="confirm button delete">Delete</a>
+					</td> 
+				</tr>
+			<?php endforeach;?>
+			</tbody>
+		</table>
 
-<table class="table-list">
-	<thead>
-		<tr>
-		    <th><?php echo lang('pyroroutes.route_name');?></th>
-		    <th><?php echo lang('pyroroutes.route_key');?></th>
-		    <th><?php echo lang('pyroroutes.route_value');?></th>
-		    <th></th>
-		</tr>
-	</thead>
-	<tbody>
-	<?php foreach ($routes as $route):?>
-		<tr>
-			<td><strong><?php echo $route->name; ?></strong></td>
-			<td><?php echo $route->route_key; ?></td>
-			<td><?php echo $route->route_value; ?></td>
-			<td class="actions"> 
-				<a href="<?php echo site_url('admin/routes/edit_route/'.$route->id);?>" class="btn orange edit"><?php echo lang('global:edit'); ?></a>
-				<a href="<?php echo site_url('admin/routes/delete_route/'.$route->id);?>" class="confirm btn red delete"><?php echo lang('global:delete'); ?></a>
-			</td> 
-		</tr>
-	<?php endforeach;?>
-	</tbody>
-</table>
+		<?php echo $pagination['links']; ?>
 
-<?php echo $pagination['links']; ?>
-
-<?php else: ?>
-	<div class="no_data"><?php echo lang('pyroroutes.no_routes');?></div>
-<?php endif;?>
-
+		<?php else: ?>
+			<div class="no_data"><?php echo lang('pyroroutes.no_routes');?></div>
+		<?php endif;?>
+	</div>
 </section>
